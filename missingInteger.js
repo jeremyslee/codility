53c47b483f1099a6af7382162c5fcc745aec9f73
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-04-18 21:35:17 -0700

    Add a solution to MissingInteger

---
+++ missingInteger.js
@@ -0,0 +1,40 @@
+/*
+Write a function:
+
+function solution(A);
+
+that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+
+For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+
+Given A = [1, 2, 3], the function should return 4.
+
+Given A = [−1, −3], the function should return 1.
+
+Assume that:
+
+N is an integer within the range [1..100,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+Copyright 2009–2018 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+*/
+
+
+function solution(A) {
+  const tempArr = [];
+  
+  for (let i = 0; i < A.length; i++) {
+      if (A[i] > 0) tempArr[A[i]] = true;
+  }
+  
+  if (tempArr.length === 0) return 1;
+  
+  for (let i = 1; i < tempArr.length; i++) {
+      if (!tempArr[i]) return i;
+  }
+  
+  return tempArr.length;
+}
